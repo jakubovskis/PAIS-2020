48f6c7864cd087b4104d5ce66064136170a3ad5d
Andris Jakubovskis <a.jakubovskis@student.rug.nl>
2021-01-06 19:08:30 +0100

    Remove not used code

--- camunda-project/src/main/MakePayment.java
+++ camunda-project/src/main/MakePayment.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
@@ -8,7 +6,6 @@ public class MakePayment implements JavaDelegate {
 	@Override
 	public void execute(DelegateExecution execution) throws Exception{
 		
-//		Random rand = new Random();
 		
 		//Decide randomly if order is accepted or not
 		execution.setVariable("paymentExecuted", true);
