1d3af83a10d87e6ceef317040f5b7385f0872612
Ruben <Gjr.kip@gmail.com>
2021-01-10 13:34:04 +0100

    working version

--- camunda-project/src/main/InformCustomer.java
+++ camunda-project/src/main/InformCustomer.java
@@ -3,27 +3,18 @@ import java.util.logging.Logger;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-public class PayRestaurant implements JavaDelegate {
+public class InformCustomer implements JavaDelegate {
 
 	private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
 	
 	@Override
 	public void execute(DelegateExecution execution) throws Exception{
 		
-		//Imagine paying here the restaurant the amount of the given price 
-		long price = (long) execution.getVariable("price");
-		
-		execution.setVariable("fullSuccess", true);
-		
+		//Here the food is being made, so just here to inform Customer of status of the process
+	    
 	    LOGGER.info("\n\n  ... LoggerDelegate invoked by "
 	            + "activtyName='" + execution.getCurrentActivityName() + "'"
 	            + ", variables=" + execution.getVariables()
 	            + " \n\n");
-		
-	    execution.getProcessEngineServices()
-	      .getRuntimeService()
-	      .createMessageCorrelation("PayedMessage")
-	      .correlate();
-	    
 	}
 }
